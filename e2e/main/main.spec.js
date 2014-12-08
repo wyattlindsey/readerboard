5e0b12151624ecc4ad64e85e74b903f982203691
Wyatt Lindsey <wyatt@newrelic.com>
2014-12-08 10:43:52 -0800

    four e2e tests written

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -3,14 +3,37 @@
 describe('Main View', function() {
   var page;
 
+  var input = element(by.model('textInput'));
+  var preview = element(by.model('readerBoardText'));
+
   beforeEach(function() {
     browser.get('/');
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  it('should allow text input', function() {
+    input.sendKeys('abc');
+    var value = input.getAttribute('value');
+    expect(value).toBe('abc');
   });
+
+  it('should coordinate text input and readerboard preview', function() {
+    input.sendKeys('ABC');
+    var value = preview.getAttribute('value');
+    expect(value).toBe('ABC');
+  });
+
+  it('should not print characters outside of those in the set', function() {
+    input.sendKeys('ü');
+    var value = preview.getAttribute('value');
+    expect(value).toBe('');
+  });
+
+  it('should capitalize lower case characters over in the preview window', function() {
+    input.sendKeys('abc');
+    var value = preview.getAttribute('value');
+    expect(value).toBe('ABC');
+  });
+
+
 });
